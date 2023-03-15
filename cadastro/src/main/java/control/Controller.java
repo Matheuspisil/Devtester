afb4c4f744a9f055e2bbf983b137d81509c842a6
Matheuspisil <ajudaserio@yahoo.com.br>
2023-03-15 15:58:58 -0300

    add pag de relatorio

--- cadastro/src/main/java/control/Controller.java
+++ cadastro/src/main/java/control/Controller.java
@@ -10,6 +10,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
 import model.DAO;
 import model.JavaBeans;
 
@@ -122,6 +128,48 @@ public class Controller extends HttpServlet {
 	}
 	protected void relatorio(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+		Document documento = new Document();
+		try {
+			response.setContentType("apllication/pdf");
+			response.addHeader("content-Disposition", "inline; filename=" + "funcionarios.pdf");
+			PdfWriter.getInstance(documento,  response.getOutputStream());
+			documento.open();
+			documento.add(new Paragraph("Lista de Funcionarios"));
+			documento.add(new Paragraph(" "));
+			PdfPTable tabela = new PdfPTable(8);
+			PdfPCell col1 = new PdfPCell(new Paragraph("ID"));
+			PdfPCell col2 = new PdfPCell(new Paragraph("Nome"));
+			PdfPCell col3 = new PdfPCell(new Paragraph("CPF")); 
+			PdfPCell col4 = new PdfPCell(new Paragraph("Cargo")); 
+			PdfPCell col5 = new PdfPCell(new Paragraph("Nascimento")); 
+			PdfPCell col6 = new PdfPCell(new Paragraph("Endereço")); 
+			PdfPCell col7 = new PdfPCell(new Paragraph("E-mail")); 
+			PdfPCell col8 = new PdfPCell(new Paragraph("Telefone")); 
+			tabela.addCell(col1);
+			tabela.addCell(col2);
+			tabela.addCell(col3);
+			tabela.addCell(col4);
+			tabela.addCell(col5);
+			tabela.addCell(col6);
+			tabela.addCell(col7);
+			tabela.addCell(col8);
+			ArrayList<JavaBeans> lista = dao.listarFuncionarios();
+			for (int i = 0; i <lista.size(); i++) {
+				tabela.addCell(lista.get(i).getIdcad());
+				tabela.addCell(lista.get(i).getNome());
+				tabela.addCell(lista.get(i).getCpf());
+				tabela.addCell(lista.get(i).getCargo());
+				tabela.addCell(lista.get(i).getNascimento());
+				tabela.addCell(lista.get(i).getEndereco());
+				tabela.addCell(lista.get(i).getEmail());
+				tabela.addCell(lista.get(i).getTel());
+			}
+			documento.add(tabela);
+			
+			documento.close();
+		} catch (Exception e) {
+			System.out.println(e);
+			documento.close();
+		}
 	}
 }
